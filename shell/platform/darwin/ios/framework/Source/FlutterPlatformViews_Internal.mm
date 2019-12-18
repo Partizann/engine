91c813b63b14fea38900f55802e8ed4db9183055
Wu Zhong <zhongwuzw@qq.com>
2019-12-19 07:32:44 +0800

    Fixes Objective-C objects memory leaks (#14326)
    Fixes CALayer memory leaks in file FlutterPlatformViews_Internal.mm,FlutterPlatformViews.mm
    
    flutter/flutter#46750

--- shell/platform/darwin/ios/framework/Source/FlutterPlatformViews_Internal.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterPlatformViews_Internal.mm
@@ -55,11 +55,10 @@ void ResetAnchor(CALayer* layer) {
 
 - (void)clipRect:(const SkRect&)clipSkRect {
   CGRect clipRect = [ChildClippingView getCGRectFromSkRect:clipSkRect];
-  CGPathRef pathRef = CGPathCreateWithRect(clipRect, nil);
-  CAShapeLayer* clip = [[CAShapeLayer alloc] init];
+  fml::CFRef<CGPathRef> pathRef(CGPathCreateWithRect(clipRect, nil));
+  CAShapeLayer* clip = [[[CAShapeLayer alloc] init] autorelease];
   clip.path = pathRef;
   self.layer.mask = clip;
-  CGPathRelease(pathRef);
 }
 
 - (void)clipRRect:(const SkRRect&)clipSkRRect {
@@ -125,7 +124,7 @@ void ResetAnchor(CALayer* layer) {
   // TODO(cyanglaz): iOS does not seem to support hard edge on CAShapeLayer. It clearly stated that
   // the CAShaperLayer will be drawn antialiased. Need to figure out a way to do the hard edge
   // clipping on iOS.
-  CAShapeLayer* clip = [[CAShapeLayer alloc] init];
+  CAShapeLayer* clip = [[[CAShapeLayer alloc] init] autorelease];
   clip.path = pathRef;
   self.layer.mask = clip;
   CGPathRelease(pathRef);
@@ -137,7 +136,7 @@ void ResetAnchor(CALayer* layer) {
     return;
   }
   if (path.isEmpty()) {
-    CAShapeLayer* clip = [[CAShapeLayer alloc] init];
+    CAShapeLayer* clip = [[[CAShapeLayer alloc] init] autorelease];
     clip.path = pathRef;
     self.layer.mask = clip;
     CGPathRelease(pathRef);
@@ -195,7 +194,7 @@ void ResetAnchor(CALayer* layer) {
     verb = iter.next(pts);
   }
 
-  CAShapeLayer* clip = [[CAShapeLayer alloc] init];
+  CAShapeLayer* clip = [[[CAShapeLayer alloc] init] autorelease];
   clip.path = pathRef;
   self.layer.mask = clip;
   CGPathRelease(pathRef);
